fcc8bb6b4e8557f1b7731fb4a644273af8d96a60
shyam514 <shyam514@users.noreply.github.com>
2026-07-22 19:16:02 +0530

    Fix import os in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
